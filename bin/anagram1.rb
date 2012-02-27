1c66b34972ae28b02bb1f3bc5be7ac60bd43f77f
Juan Carlos Crespo <alu2956@etsii.ull.es>
2012-02-27 16:26:35 +0000

    añadido el resto de código para anagram1.rb

--- bin/anagram1.rb
+++ bin/anagram1.rb
@@ -24,3 +24,19 @@ def signature_of(word)
   word.unpack("c*").sort.pack("c*")
 end
 
+signatures = Hash.new { |h,k| h[k] = [] }
+File.foreach(dictionary) do |line|
+  word = line.chomp
+  signature = signature_of(word)
+  signatures[signature] << word
+end
+
+
+ARGV.each do |word|
+  s = signature_of(word)
+  if signatures[s].length != 0
+    puts "Anagrams of '#{word}': #{signatures[s].join(', ')}"
+  else
+    puts "No anagrams of '#{word}' found in #{dictionary}"
+  end
+end
